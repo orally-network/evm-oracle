c4911017a3859281d3a3e21b913ea8dc479f8b0f
verkhohliad <ihor.verkhohliad@gmail.com>
2024-01-18 08:41:10 +0000

    update deployment scripts

--- script/Deploy_1.s.sol
+++ script/Deploy_1.s.sol
@@ -11,6 +11,7 @@ import {Multicall} from "src/Multicall.sol";
 contract Deploy_1 is Script {
     address constant pmaAddress = 0x05C3F2A3Ae0b7f3775044EEFED8a864c47125F19;
     address constant sybilAddress = 0x60825063CB0f4EF508854Ad4913f3a6de86B3807;
+    address constant amaAddress = 0x000000000000000000000000000000000000dEaD;
 
     function run() public {
         console2.log("Running deploy script for the Factory contract");
@@ -30,6 +31,9 @@ contract Deploy_1 is Script {
         registry.initialize();
         registry.add(keccak256("PYTHIA_EXECUTOR"), pmaAddress);
         registry.add(keccak256("PYTHIA_EXECUTOR"), address(multi));
+
+        registry.add(keccak256("APOLLO_EXECUTOR"), amaAddress);
+        registry.add(keccak256("APOLLO_EXECUTOR"), address(multi));
 
         vm.stopBroadcast();
     }
