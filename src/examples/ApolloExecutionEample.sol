0fb375714894ee97f6ff1757d03fe703a9586168
verkhohliad <ihor.verkhohliad@gmail.com>
2024-02-20 17:15:04 +0000

    add ApolloConSumerExample

---
+++ src/examples/ApolloExecutionEample.sol
@@ -0,0 +1,23 @@
+import {OrallyApolloConsumer} from "../consumers/OrallyApolloConsumer.sol";
+import {IApolloCoordinator} from "../interfaces/IApolloCoordinator.sol";
+
+contract ApolloConsumerExample is OrallyApolloConsumer {
+    uint256 public rate;
+    uint256 public decimals;
+    uint256 public timestamp;
+    IApolloCoordinator public apollo;
+
+    constructor(address _executorsRegistry, address _apolloCoordinator) OrallyApolloConsumer(_executorsRegistry) {
+        apollo = IApolloCoordinator(_apolloCoordinator);
+    }
+
+    function requestValue() public {
+        apollo.requestDataFeed("ICP/USD", 300000);
+    }
+
+    function fulfillDataFeed(string memory, uint256 _rate, uint256 _decimals, uint256 _timestamp) external onlyExecutor {
+        rate = _rate;
+        decimals = _decimals;
+        timestamp = _timestamp;
+    }
+}
