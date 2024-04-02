061186f804c9c15599399595cdddb3ebc6963604
verkhohliad <ihor.verkhohliad@gmail.com>
2024-04-02 19:06:18 +0100

    change apollo example, add new deployment scripts

--- script/Deploy_ApolloConsumerExample.s.sol
+++ script/Deploy_ApolloConsumerExample.s.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import {console2} from "forge-std/console2.sol";
 
-import {RaffleExample} from "src/examples/RaffleExample.sol";
+import {ApolloConsumerExample} from "src/examples/ApolloConsumerExample.sol";
 
-contract Deploy_RaffleExample is Script {
+contract Deploy_ApolloConsumerExample is Script {
     address constant pythiaExecutorRegistry = 0x81f8573B46895f65C7658Aa3A0eB90578F7F2dC9;
     address constant apolloCoordinator = 0x1FEa4E134c8BcDF6E1323C1Bf2Aa0899049CC754;
 
@@ -16,8 +16,8 @@ contract Deploy_RaffleExample is Script {
         console2.log("Running deploy script for the Factory contract");
         vm.startBroadcast();
 
-        RaffleExample raffle = new RaffleExample(pythiaExecutorRegistry, apolloCoordinator, 100, TICKET_PRICE);
-        console2.log("RaffleExample deployed at:", address(raffle));
+        ApolloConsumerExample consumer = new ApolloConsumerExample(pythiaExecutorRegistry, apolloCoordinator);
+        console2.log("ApolloConsumerExample deployed at:", address(consumer));
 
         vm.stopBroadcast();
     }
