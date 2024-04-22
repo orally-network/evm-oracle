7f5af5b4f9d9695d85528a6bdc4f593ce9e96cef
verkhohliad <ihor.verkhohliad@gmail.com>
2024-04-22 20:00:43 +0100

    add relevant interfaces, consumers, receivers to orally-sdk-solidity lib

---
+++ orally-sdk-solidity/ApolloReceiver.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity 0.8.20;
+
+import {OrallyApolloConsumer} from "./OrallyApolloConsumer.sol";
+import {IApolloCoordinator} from "./IApolloCoordinator.sol";
+
+/**
+ * @title ApolloReceiver
+ * @dev Inherits from OrallyApolloConsumer to create a contract capable of receiving data from the Orally oracle network.
+ * This contract acts as a template for contracts that want to receive data from the Orally oracle,
+ * with customizable handling of different data types and structures.
+ */
+abstract contract ApolloReceiver is OrallyApolloConsumer {
+    IApolloCoordinator public apolloCoordinator;
+
+    /**
+     * @dev Constructor that initializes the ApolloReceiver contract with a specific registry address.
+     * @param _registry Address of the registry contract in the Orally network.
+     */
+    constructor(address _registry, address _apolloCoordinator) OrallyApolloConsumer(_registry) {
+        apolloCoordinator = IApolloCoordinator(_apolloCoordinator);
+    }
+
+    /**
+     * @notice Abstract function to be implemented by inheriting contracts for processing received oracle data.
+     * @dev This function needs to be overridden in child contracts to specify how the received data should be processed.
+     * @param data The data sent from the oracle, expected to be encoded in a predefined format.
+     */
+    function fulfillData(bytes memory data) internal virtual;
+
+    /**
+     * @notice Receives data from the Orally oracle and forwards it to the internal 'fulfillData' function.
+     * @dev Ensures that only an authorized executor (likely the Orally oracle) can call this function.
+     * This function acts as a secure entry point for data delivered by the oracle.
+     * @param data The data being passed from the oracle, which can be encoded in various formats depending on the use case.
+     */
+    function rawFulfillData(bytes memory data) external onlyExecutor {
+        fulfillData(data);
+    }
+}
---
+++ orally-sdk-solidity/IOrallyVerifierOracle.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.20;
+
+/**
+ * @title IOrallyVerifierOracle
+ * @notice Interface for the OrallyVerifierOracle contract.
+ * This interface defines the required functions for verifying and handling
+ * data feeds, specifically focusing on functionality related to price feeds
+ * and custom data verification processes.
+ */
+interface IOrallyVerifierOracle {
+    // Events to notify about changes in reporter status or data updates
+    event ReporterAdded(address indexed reporter);
+    event ReporterRemoved(address indexed reporter);
+    event PriceFeedSaved(string indexed pairId, uint256 price, uint256 decimals, uint256 timestamp);
+
+    /**
+     * @notice Verifies the authenticity of a packed message using a signature.
+     * @param _message The hash of the message being verified.
+     * @param _signature The digital signature associated with the message.
+     * @return bool Returns true if the signature is valid and from a trusted source.
+     */
+    function verifyPacked(bytes32 _message, bytes memory _signature) external view returns (bool);
+
+    /**
+     * @notice Verifies the authenticity and integrity of unpacked data elements with their signature.
+     * @param _pairId The identifier for the currency pair.
+     * @param _price The latest reported price for the currency pair.
+     * @param _decimals The number of decimal places for the reported price.
+     * @param _timestamp The timestamp when the data was signed.
+     * @param _signature The digital signature covering the data.
+     * @return bool Returns true if the signature is valid and from an authorized source.
+     */
+    function verifyUnpacked(
+        string memory _pairId,
+        uint256 _price,
+        uint256 _decimals,
+        uint256 _timestamp,
+        bytes memory _signature
+    ) external view returns (bool);
+
+    /**
+     * @notice Unpacks a message containing price feed data.
+     * @param _message The packed message containing encoded price feed data.
+     * @return Tuple containing the pair ID, price, decimals, and timestamp extracted from the message.
+     */
+    function unpack(bytes32 _message) external pure returns (string memory, uint256, uint256, uint256);
+
+    /**
+     * @notice Checks if an address is an authorized reporter.
+     * @param _reporter The address to check.
+     * @return bool Returns true if the address is authorized to submit data.
+     */
+    function isReporter(address _reporter) external view returns (bool);
+
+    /**
+     * @notice Verifies and returns the details of a price feed from provided data.
+     * @param data The packed data containing the price feed and its signature.
+     * @return Tuple of pair ID, price, decimals, and timestamp if the verification is successful.
+     */
+    function verifyPriceFeed(bytes memory data) external view returns (string memory, uint256, uint256, uint256);
+
+    /**
+     * @notice Verifies, caches, and returns the details of a price feed.
+     * Caching is performed to store the most recent and valid data.
+     * @param data The packed data containing the price feed and its signature.
+     * @return Tuple of pair ID, price, decimals, and timestamp.
+     */
+    function verifyPriceFeedWithCache(bytes calldata data) external returns (string memory, uint256, uint256, uint256);
+
+    /**
+     * @notice Verifies and returns custom numerical data from provided packed data.
+     * @param data The packed data containing custom numerical information and its signature.
+     * @return Tuple containing the feed ID, numerical value, and decimals.
+     */
+    function verifyCustomNumber(bytes calldata data) external returns (string memory, uint256, uint256);
+
+    /**
+     * @notice Verifies and returns custom string data from provided packed data.
+     * @param data The packed data containing custom string information and its signature.
+     * @return Tuple containing the feed ID and the string value.
+     */
+    function verifyCustomString(bytes calldata data) external returns (string memory, string memory);
+
+    /**
+     * @notice Verifies and returns the details of a chain data feed from provided data.
+     * @param data The packed data containing the chain data feed and its signature.
+     * @return Tuple of chainData and metaData if the verification is successful.
+     */
+    function verifyChainData(bytes calldata data) external returns (bytes memory, bytes memory);
+}
