3fd6963b41e449cb1dcdc72b21ede31c9da5254a
verkhohliad <ihor.verkhohliad@gmail.com>
2024-04-22 15:45:43 +0100

    add methods for verification price feeds, custom string and number

--- src/sybil/OrallyVerifierOracle.sol
+++ src/sybil/OrallyVerifierOracle.sol
@@ -5,30 +5,52 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {IOrallyVerifierOracle} from "./IOrallyVerifierOracle.sol";
 
+import {console2} from "@forge-std/console2.sol";
+
 /**
  * @title OrallyVerifierOracle
- * @notice Used to verify prices from Orally API
+ * @notice This contract is used to verify and cache data feeds from the Orally's Sybil canister.
+ * It handles verification of signed data, unpacks and caches it for quick access.
+ * The contract uses ECDSA for cryptographic operations and inherits from Ownable for access control.
  */
 contract OrallyVerifierOracle is Ownable, IOrallyVerifierOracle {
     using ECDSA for bytes32;
 
+    // Structure to store information about each price feed
     struct PriceFeed {
-        string pairId;
-        uint256 price;
-        uint256 decimals;
-        uint256 timestamp;
+        string pairId;     // The identifier for the currency pair
+        uint256 price;     // The latest price of the currency pair
+        uint256 decimals;  // The decimal places for the price to ensure precision
+        uint256 timestamp; // The timestamp when the price was last updated
     }
 
+    // Mapping to track authorized reporters who can sign and submit price feeds (Sybil permissionless wallet)
     mapping(address => bool) public reporters;
 
+    // Mapping to store the latest price feeds by pair ID
     mapping(string => PriceFeed) public priceFeeds;
 
     constructor(address owner) Ownable(owner) {}
 
+    /**
+     * @notice Verifies if a packed message signature is valid and from an authorized reporter.
+     * @param _message The hash of the signed message.
+     * @param _signature The digital signature of the message.
+     * @return bool Returns true if the signature is valid and the signer is authorized.
+     */
     function verifyPacked(bytes32 _message, bytes memory _signature) public view returns (bool) {
         return reporters[ECDSA.recover(_message, _signature)];
     }
 
+    /**
+     * @notice Verifies the unpacked data elements with their signature.
+     * @param _pairId The currency pair identifier.
+     * @param _price The price of the currency pair.
+     * @param _decimals The number of decimals for the price.
+     * @param _timestamp The timestamp of the price update.
+     * @param _signature The digital signature covering the data.
+     * @return bool Returns true if the signature is valid and the data is from an authorized reporter.
+     */
     function verifyUnpacked(
         string memory _pairId,
         uint256 _price,
@@ -39,35 +61,86 @@ contract OrallyVerifierOracle is Ownable, IOrallyVerifierOracle {
         return verifyPacked(keccak256(abi.encodePacked(_pairId, _price, _decimals, _timestamp)), _signature);
     }
 
-    function unpack(bytes32 _message) public pure returns (string memory, uint256, uint256, uint256) {
+    /**
+     * @notice Extracts and returns price feed data from a packed message.
+     * @param _message The packed message containing encoded price feed data.
+     * @return Tuple containing the pair ID, price, decimals, and timestamp extracted from the message.
+     */
+    function unpack(bytes32 _message) external pure returns (string memory, uint256, uint256, uint256) {
         return abi.decode(abi.encodePacked(_message), (string, uint256, uint256, uint256));
     }
 
+    /**
+     * @notice Verifies the integrity and authenticity of price feed data, then returns it.
+     * @param data The packed byte array containing the price feed and its signature.
+     * @return Tuple of pair ID, price, decimals, and timestamp if the verification is successful.
+     */
     function verifyPriceFeed(bytes memory data) public view returns (string memory, uint256, uint256, uint256) {
         (string memory pairId, uint256 price, uint256 decimals, uint256 timestamp, bytes memory signature) = abi.decode(data, (string, uint256, uint256, uint256, bytes));
-
         require(verifyUnpacked(pairId, price, decimals, timestamp, signature), "Invalid signature");
-
         return (pairId, price, decimals, timestamp);
     }
 
-    function verifyPriceFeedWithCache(bytes memory data) public returns (string memory, uint256, uint256, uint256) {
+    /**
+     * @notice Verifies, caches, and returns the details of a price feed.
+     * Caching is performed to store the most recent and valid data.
+     * @param data The packed data containing the price feed and its signature.
+     * @return Tuple of pair ID, price, decimals, and timestamp.
+     */
+    function verifyPriceFeedWithCache(bytes memory data) external returns (string memory, uint256, uint256, uint256) {
         (string memory pairId, uint256 price, uint256 decimals, uint256 timestamp) = verifyPriceFeed(data);
-
         priceFeeds[pairId] = PriceFeed(pairId, price, decimals, timestamp);
-
+        emit PriceFeedSaved(pairId, price, decimals, timestamp);
         return (pairId, price, decimals, timestamp);
     }
 
+    /**
+     * @notice Verifies and returns custom numerical data from provided packed data.
+     * @param data The packed data containing custom numerical information and its signature.
+     * @return Tuple containing the feed ID, numerical value, and decimals.
+     */
+    function verifyCustomNumber(bytes memory data) external view returns (string memory, uint256, uint256) {
+        (string memory feedId, uint256 number, uint256 decimals, bytes memory signature) = abi.decode(data, (string, uint256, uint256, bytes));
+
+        require(verifyPacked(keccak256(abi.encodePacked(feedId, number, decimals)), signature), "Invalid signature");
+
+        return (feedId, number, decimals);
+    }
+
+    /**
+     * @notice Verifies and returns custom string data from provided packed data.
+     * @param data The packed data containing custom string information and its signature.
+     * @return Tuple containing the feed ID and the string value.
+     */
+    function verifyCustomString(bytes memory data) external view returns (string memory, string memory) {
+        (string memory feedId, string memory value, bytes memory signature) = abi.decode(data, (string, string, bytes));
+
+        require(verifyPacked(keccak256(abi.encodePacked(feedId, value)), signature), "Invalid signature");
+
+        return (feedId, value);
+    }
+
+    /**
+     * @notice Checks if an address is an authorized reporter.
+     * @param _reporter The address to check.
+     */
     function isReporter(address _reporter) external view returns (bool) {
         return reporters[_reporter];
     }
 
+    /**
+     * @notice Adds a new reporter and authorizes them to submit signed data.
+     * @param _reporter The address of the reporter to add.
+     */
     function addReporter(address _reporter) external onlyOwner {
         reporters[_reporter] = true;
         emit ReporterAdded(_reporter);
     }
 
+    /**
+     * @notice Removes an authorized reporter.
+     * @param _reporter The address of the reporter to remove.
+     */
     function removeReporter(address _reporter) external onlyOwner {
         reporters[_reporter] = false;
         emit ReporterRemoved(_reporter);
--- src/sybil/test/OrallyVerifierOracle.test.sol
+++ src/sybil/test/OrallyVerifierOracle.test.sol
@@ -7,27 +7,85 @@ import {OrallyVerifierOracle} from "../OrallyVerifierOracle.sol";
 
 import {console2} from "@forge-std/console2.sol";
 
-
 contract OrallyVerifierOracleTest is Test {
-    address constant reporter = 0xBFD54D868BE89184f19f597489A9FA9385AA708e;
+    // random deployer address for testing
+    address constant deployer = 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38;
+
+    // Sybil permissionless wallet (staging)
+    address constant stagingReporter = 0xBFD54D868BE89184f19f597489A9FA9385AA708e;
+
+    /*
+    source (staging): https://tysiw-qaaaa-aaaak-qcikq-cai.icp0.io/get_xrc_data_with_proof?id=DOGE/SHIB&bytes=true
+    decoded data: {
+      "data": {
+        "DefaultPriceFeed": {
+          "symbol": "DOGE/SHIB",
+          "rate": 6767282465616,
+          "decimals": 9,
+          "timestamp": 1713285660
+        }
+      },
+      "signature": "e11a43af066346343576d6e9c8a8945bdc9d76cf25c07f8e52624355480a648a49be00b980cd043bc994d47d6c1256b405d43e13610d51726de585786e0e41aa1c"
+    }
+    */
+    bytes constant priceFeedData = hex"00000000000000000000000000000000000000000000000000000000000000a000000000000000000000000000000000000000000000000000000627a177ab50000000000000000000000000000000000000000000000000000000000000000900000000000000000000000000000000000000000000000000000000661eaa1c00000000000000000000000000000000000000000000000000000000000000e00000000000000000000000000000000000000000000000000000000000000009444f47452f5348494200000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000041e59c98fffd7aa182f5015f83dd9d11a30ece01f675efb7b4a491fc6e51c65fd2759c32ef9708b6262338d7027339ad3aa6a2819ad08be5dbd37898a5b5849beb1b00000000000000000000000000000000000000000000000000000000000000";
+
+    /*
+    source (staging): https://tysiw-qaaaa-aaaak-qcikq-cai.icp0.io/get_feed_data_with_proof?id=custom_BTC/USD_NUMBER&bytes=true
+    decoded data: {
+      "data": {
+        "CustomNumber": {
+          "id": "custom_BTC/USD_NUMBER",
+          "value": 61854000000,
+          "decimals": 6
+        }
+      },
+      "signature": "49500f346ee88e09aa78c62cc0cf499ba9628521c14e750224335ec3bb4d96eb1029eb79c10e7fd2d0737c9da52fef43f404dc6447fcbf3b4d90b46d66649ed61b"
+    }
+    */
+    bytes constant customNumberData = hex"00000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000e66c92380000000000000000000000000000000000000000000000000000000000000000600000000000000000000000000000000000000000000000000000000000000c00000000000000000000000000000000000000000000000000000000000000015637573746f6d5f4254432f5553445f4e554d4245520000000000000000000000000000000000000000000000000000000000000000000000000000000000004149500f346ee88e09aa78c62cc0cf499ba9628521c14e750224335ec3bb4d96eb1029eb79c10e7fd2d0737c9da52fef43f404dc6447fcbf3b4d90b46d66649ed61b00000000000000000000000000000000000000000000000000000000000000";
+
+    /*
+    source (staging): https://tysiw-qaaaa-aaaak-qcikq-cai.icp0.io/get_asset_data_with_proof?id=custom_get_logs_example&bytes=true
+    decoded data: {
+      "data": {
+        "CustomString": {
+          "id": "custom_get_logs_example",
+          "value": "ETH/USD"
+        }
+      },
+      "signature": "b2df8f098a5ce1c9a9d89df1b6d7d46d3bdfafca60fa2b6f043759e6d7b9f75c67c2e251058fd86d2c88206ada8cf2289b361601304da7ff0053eb0782d496f91c"
+    }
+    */
+    bytes constant customStringData = hex"000000000000000000000000000000000000000000000000000000000000006000000000000000000000000000000000000000000000000000000000000000a000000000000000000000000000000000000000000000000000000000000000e00000000000000000000000000000000000000000000000000000000000000017637573746f6d5f6765745f6c6f67735f6578616d706c6500000000000000000000000000000000000000000000000000000000000000000000000000000000074554482f555344000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000041b2df8f098a5ce1c9a9d89df1b6d7d46d3bdfafca60fa2b6f043759e6d7b9f75c67c2e251058fd86d2c88206ada8cf2289b361601304da7ff0053eb0782d496f91c00000000000000000000000000000000000000000000000000000000000000";
 
     uint256 fork;
     OrallyVerifierOracle verifier;
+    uint256 reporterKey = 0xa11ce;
+    address reporter;
 
     function setUp() public {
 //        fork = vm.createFork("https://mainnet.infura.io/v3/{key}");
 //        vm.selectFork(fork);
 
-        console2.log("msg.sender:", msg.sender);
+        reporter = vm.addr(reporterKey);
 
+        console2.log("Reporter address: {}", reporter);
+
+        vm.startPrank(deployer);
         verifier = new OrallyVerifierOracle(msg.sender);
-
         verifier.addReporter(reporter);
+        verifier.addReporter(stagingReporter);
+    }
+
+    function testIsReporter() public {
+        assertTrue(verifier.isReporter(reporter), "The reporter should be authorized.");
     }
 
     function testAddReporter() public {
-        verifier.addReporter(reporter);
-        assertEq(verifier.isReporter(reporter), true);
+        address newReporter = vm.addr(0x1234);
+        verifier.addReporter(newReporter);
+        assertTrue(verifier.isReporter(newReporter), "The reporter should be authorized.");
     }
 
     function testRemoveReporter() public {
@@ -35,13 +93,59 @@ contract OrallyVerifierOracleTest is Test {
         assertEq(verifier.isReporter(reporter), false);
     }
 
-    function testVerifyPriceFeed() public {
-        (string memory pairId, uint256 price, uint256 decimals, uint256 timestamp) = verifier.verifyPriceFeed("0x00000000000000000000000000000000000000000000000000000000000000a000000000000000000000000000000000000000000000000000000000bd3564e80000000000000000000000000000000000000000000000000000000000000009000000000000000000000000000000000000000000000000000000006617f89800000000000000000000000000000000000000000000000000000000000000e000000000000000000000000000000000000000000000000000000000000000085852502f444f47450000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
+    function testVerifyPacked() public {
+        // Simulating the creation of a valid signature
+        bytes32 message = keccak256("Test message");
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(reporterKey, message); // Using Foundry's vm.sign to simulate reporter signing the message
+        bytes memory signature = abi.encodePacked(r, s, v);
 
-        console2.log("pairId:", pairId);
-        console2.log("price:", price);
-        console2.log("decimals:", decimals);
-        console2.log("timestamp:", timestamp);
+        assertTrue(verifier.verifyPacked(message, signature), "The signature should be valid.");
     }
 
+    function testVerifyUnpacked() public {
+        // Encoding data as expected by the verifyUnpacked function
+        string memory pairId = "ETH/USD";
+        uint256 price = 4000000000000000000000;
+        uint256 decimals = 18;
+        uint256 timestamp = block.timestamp;
+        bytes32 dataHash = keccak256(abi.encodePacked(pairId, price, decimals, timestamp));
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(reporterKey, dataHash); // Sign the hash of the data
+        bytes memory signature = abi.encodePacked(r, s, v);
+
+        assertTrue(verifier.verifyUnpacked(pairId, price, decimals, timestamp, signature), "The unpacked data should verify correctly.");
+    }
+
+    function testVerifyPriceFeed() public {
+        (string memory pairId, uint256 price, uint256 decimals, uint256 timestamp) = verifier.verifyPriceFeed(priceFeedData);
+
+        assertEq(pairId, "DOGE/SHIB");
+        assertEq(price, 6767282465616);
+        assertEq(decimals, 9);
+        assertEq(timestamp, 1713285660);
+    }
+
+    function testVerifyPriceFeedWithCache() public {
+        (string memory pairId, uint256 price, uint256 decimals, uint256 timestamp) = verifier.verifyPriceFeedWithCache(priceFeedData);
+
+        assertEq(pairId, "DOGE/SHIB");
+        assertEq(price, 6767282465616);
+        assertEq(decimals, 9);
+        assertEq(timestamp, 1713285660);
+    }
+
+    function testVerifyCustomNumber() public {
+        (string memory id, uint256 value, uint256 decimals) = verifier.verifyCustomNumber(customNumberData);
+
+        assertEq(id, "custom_BTC/USD_NUMBER");
+        assertEq(value, 61854000000);
+        assertEq(decimals, 6);
+    }
+
+    function testVerifyCustomString() public {
+        (string memory id, string memory value) = verifier.verifyCustomString(customStringData);
+
+        assertEq(id, "custom_get_logs_example");
+        assertEq(value, "ETH/USD");
+    }
 }
