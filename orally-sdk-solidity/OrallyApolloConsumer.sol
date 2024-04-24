64419bdb903e7f1de43dc87d560014c10ae76360
verkhohliad <ihor.verkhohliad@gmail.com>
2024-04-24 14:19:42 +0100

    add pack script for solidity-sdk

--- orally-sdk-solidity/OrallyApolloConsumer.sol
+++ orally-sdk-solidity/OrallyApolloConsumer.sol
@@ -1,4 +1,23 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.20;
 
-import "../src/apollo/OrallyApolloConsumer.sol";
+import {IOrallyExecutorsRegistry} from "../registry/IOrallyExecutorsRegistry.sol";
+
+contract OrallyApolloConsumer {
+    IOrallyExecutorsRegistry private registry;
+
+    constructor(address _registry) {
+        registry = IOrallyExecutorsRegistry(_registry);
+    }
+
+    function isExecutor(address _addr) public view returns (bool) {
+        return registry.isApolloExecutor(_addr);
+    }
+
+    modifier onlyExecutor() {
+        if (!registry.isApolloExecutor(msg.sender)) revert ApolloCallerUnauthorized(msg.sender);
+        _;
+    }
+
+    error ApolloCallerUnauthorized(address caller);
+}
--- orally-sdk-solidity/OrallyStructs.sol
+++
@@ -1,4 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0
-pragma solidity ^0.8.20;
-
-import "../src/OrallyStructs.sol";
